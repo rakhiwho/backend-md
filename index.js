faa52a7e88e004fd5e996e9034dea987a750a63e
rakhiwho <149473696+rakhiwho@users.noreply.github.com>
2024-11-07 00:09:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-// src/index.js
-const express = require('express');
+import express from 'express';
 const app = express();
 
-// Middleware (optional, add as needed)
-app.use(express.json());
-
-// Import routes from the routes folder
-const userRoutes = require('./routes/user');
-app.use('/user', userRoutes);  // All /user routes will be handled in user.js
-
-// Root route
 app.get('/', (req, res) => {
   res.send('Hello from Vercel!');
 });
 
-// Export the Express app for Vercel
-module.exports = app;
+export default app;
