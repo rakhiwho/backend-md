42a7011ed58264b34f4982ec297f8eae3fd2b609
rakhiwho <149473696+rakhiwho@users.noreply.github.com>
2024-11-10 20:19:23 +0530

    Update socket.js

--- src/socket.io/socket.js
+++ src/socket.io/socket.js
@@ -13,16 +13,7 @@ const server = http.createServer(app);
  const allowedOriginRegex = /^https:\/\/media-4ba1(-[a-zA-Z0-9]+)?\.vercel\.app$/;
 const io = new Server(server, {
  cors: {
-    origin:(origin, callback) => {
-      if (!origin || allowedOriginRegex.test(origin)) {
-        callback(null, true); // Allow the request if it matches the regex or has no origin (for server-to-server requests)
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    }, 
-    methods: ['GET', 'POST'],
-    credentials: true,
-  },
+    origin: "https://media-bice.vercel.app",
 });
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*'); // Alternatively, use the allowedOriginRegex here for added security.
